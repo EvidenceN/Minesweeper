efaa00a0a5e7ac7030cbf61ec6785d73f22a3c32
EvidenceN <Evscytech@gmail.com>
2018-07-21 03:01:27 -0500

    minesweeper3.js project done

--- src/minesweeper3.js
+++ src/minesweeper3.js
@@ -10,5 +10,36 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board;
 };
 
-generatePlayerBoard(3, 3);
-console.log(generatePlayerBoard(5, 2));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let numberOfRowsIndex = 1; numberOfRowsIndex < numberOfRows.length; numberOfRows++) {
+    let row = [];
+    for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns.length; numberOfColumns++) {
+      row.push(null);
+    };
+    board.push(row);
+  };
+
+  let numberOfBombsPlaced = 0; {
+    while (numberOfBombsPlaced < numberOfBombs);
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+  /* The code in this while loop has the potential to place bombs on top of already existing bombs.
+  This will be fixed when i apply control flow*/
+  };
+
+  return board;
+};
+
+const printBoard = (board) => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3 , 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board');
+printBoard(bombBoard);
